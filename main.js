1fa5b1eec11e538773ba62832cb88e1c34325512
WM Chang <wandererm@gmail.com>
2015-07-25 00:03:00 +0800

    add main.js and index.html

---
+++ main.js
@@ -0,0 +1,59 @@
+var ipc = require('ipc');
+var net = require('net');
+
+var telnet = new net.Socket();
+
+ipc.on('send', function(event, arg) {
+	telnet.write(arg);
+});
+
+ipc.on('connect', function(event, site){
+	telnet.connect(23, site, function() {
+		console.log('TELNET START');
+	});
+});
+
+var app = require('app');  // Module to control application life.
+var BrowserWindow = require('browser-window');  // Module to create native browser window.
+
+// Report crashes to our server.
+//require('crash-reporter').start();
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the javascript object is GCed.
+var mainWindow = null;
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function() {
+  if (process.platform != 'darwin')
+    app.quit();
+});
+
+// This method will be called when atom-shell has done everything
+// initialization and ready for creating browser windows.
+app.on('ready', function() {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    'min-width': 720,
+    'min-height': 640,
+    'web-preferences': {
+		'web-security': false
+	}
+  });
+
+  // and load the index.html of the app.
+  	mainWindow.loadUrl('file://' + __dirname + '/index.html');
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function() {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
+
+	telnet.on('data', function(data) {
+		mainWindow.webContents.send('data', data);
+	});
+	//mainWindow.toggleDevTools();
+});
